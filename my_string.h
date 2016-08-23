457ed5a0404e269d46b7f428d98757e503d8065b
yoopibit <yavorskyi.yurii@gmail.com>
2016-08-24 00:58:43 +0300

    update string functions

--- my_string.h
+++ my_string.h
@@ -2,6 +2,10 @@
 #define MY_STRING_H
 #include <inttypes.h>
 
+#if !defined(size_t) || !defined(NULL)
+#define size_t unsigned long
+#endif
+
 char* long_to_str(long i, char* str, size_t lenght);
 char* double_to_str(double number, char* str, size_t lenght, uint8_t after_coma = 3);
 
