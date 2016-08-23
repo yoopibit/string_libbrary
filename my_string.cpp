7591dd9e343493f70771859bcacbe55aff9b65c6
yoopibit <yavorskyi.yurii@gmail.com>
2016-08-24 00:21:27 +0300

    added double to string function

--- my_string.cpp
+++ my_string.cpp
@@ -1,7 +1,5 @@
 #include "my_string.h"
 
-#define my_abs(num) ((num) < 0 ? -(num) : (num))
-
 static int lenght_number(long number);
 
 char* long_to_str(long num, char* str, size_t lenght) {
@@ -10,12 +8,12 @@ char* long_to_str(long num, char* str, size_t lenght) {
     if (lenght <= current_lenght)
         return 0;
 
-    if (num < 0)
+    if (num < 0) {
         str[0] = '-';
-
+        num = -num;
+    }
     int compare_value = ((num < 0) ? 1 : 0);
-    num = my_abs(num);
-
+    
     for (i = current_lenght - 1; i >= compare_value; --i) {
         str[i] = '0' + num % 10;
         num /= 10;
@@ -23,7 +21,30 @@ char* long_to_str(long num, char* str, size_t lenght) {
     str[current_lenght] = '\0';
     return str;
 }
+char* double_to_str(double number, char* str, size_t lenght, uint8_t after_coma = 3) {
+    const size_t current_lenght = lenght_number((int)number) + after_coma + 1;
 
+    if (str == NULL ||  current_lenght >= lenght)
+        return NULL;
+
+    const int compare_value = ((number < 0) ? 1 : 0);
+    if (number < 0) {
+        str[0] = '-';
+        number = -number;
+    }
+    str[current_lenght - after_coma - 1] = ',';
+
+    for(int i = current_lenght - after_coma - 2, copy_number = (int)number; i >= compare_value; --i) {
+        str[i] = '0' + copy_number % 10;;
+        copy_number /= 10;
+    }
+    for (int i = current_lenght - after_coma; i < current_lenght; ++i) {
+        number *= 10;
+        str[i] = '0' + (uint64_t)(number) % 10;
+    }
+    str[current_lenght] = '\0';
+    return str;
+}
 
 static int lenght_number(long number) {
     int lenght = number < 0 ? 1 : 0;
--- my_string.h
+++ my_string.h
@@ -1,10 +1,8 @@
 #ifndef MY_STRING_H
 #define MY_STRING_H
-
-#ifndef size_t
-#define size_t unsigned int
-#endif
+#include <inttypes.h>
 
 char* long_to_str(long i, char* str, size_t lenght);
+char* double_to_str(double number, char* str, size_t lenght, uint8_t after_coma = 3);
 
 #endif // MY_STRING_H
